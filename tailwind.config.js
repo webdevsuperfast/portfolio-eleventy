bcebf2d70ccc98abaa4548e2513c341c9050ea7d
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-02-11 21:36:38 +0800

    Added additional font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,10 +19,25 @@ module.exports = {
         'barlow-condensed': ['Barlow Condensed', 'sans-serif'],
       },
       fontSize: {
+        '10xl': ['10rem', {
+          lineHeight: '1'
+        }],
+        '12xl': ['12rem', {
+          lineHeight: '1'
+        }],
         '14xl': ['14rem', {
           lineHeight: '1'
         }],
         '16xl': ['16rem', {
+          lineHeight: '1'
+        }],
+        '18xl': ['18rem', {
+          lineHeight: '1'
+        }],
+        '20xl': ['20rem', {
+          lineHeight: '1'
+        }],
+        '22xl': ['22rem', {
           lineHeight: '1'
         }]
       },
