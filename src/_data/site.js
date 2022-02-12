44ef5292efd453b9c55a720b0d3f3f494f4dab30
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-02-12 10:17:07 +0800

    Added Google & Microsoft verification

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,8 @@
+const { GOOGLE_SITE_VERIFICATION, MICROSOFT_SITE_VERIFICATION } = require('../../env');
+
 module.exports = {
   'title': 'Rotsen Mark Acob - Jamstack Developer',
   'description': 'Web developer, self-proclaimed introvert and UNIX enthusiast.',
+  'google_site_verification': GOOGLE_SITE_VERIFICATION,
+  'microsoft_site_verification': MICROSOFT_SITE_VERIFICATION,
 }
