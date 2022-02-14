2ee51bd43ab79ad21b0597edc7a3d46645ed614d
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-02-14 08:19:52 +0800

    Prettifier changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,35 +16,57 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        'barlow': ['Barlow', 'sans-serif'],
+        barlow: ['Barlow', 'sans-serif'],
         'barlow-condensed': ['Barlow Condensed', 'sans-serif'],
       },
       fontSize: {
-        '10xl': ['10rem', {
-          lineHeight: '1'
-        }],
-        '12xl': ['12rem', {
-          lineHeight: '1'
-        }],
-        '14xl': ['14rem', {
-          lineHeight: '1'
-        }],
-        '16xl': ['16rem', {
-          lineHeight: '1'
-        }],
-        '18xl': ['18rem', {
-          lineHeight: '1'
-        }],
-        '20xl': ['20rem', {
-          lineHeight: '1'
-        }],
-        '22xl': ['22rem', {
-          lineHeight: '1'
-        }]
+        '10xl': [
+          '10rem',
+          {
+            lineHeight: '1',
+          },
+        ],
+        '12xl': [
+          '12rem',
+          {
+            lineHeight: '1',
+          },
+        ],
+        '14xl': [
+          '14rem',
+          {
+            lineHeight: '1',
+          },
+        ],
+        '16xl': [
+          '16rem',
+          {
+            lineHeight: '1',
+          },
+        ],
+        '18xl': [
+          '18rem',
+          {
+            lineHeight: '1',
+          },
+        ],
+        '20xl': [
+          '20rem',
+          {
+            lineHeight: '1',
+          },
+        ],
+        '22xl': [
+          '22rem',
+          {
+            lineHeight: '1',
+          },
+        ],
       },
       backgroundImage: {
-        body: 'url(../images/background.jpg)'
-      }
+        light: 'url(../images/background-light.jpg)',
+        dark: 'url(../images/background-dark.jpg)',
+      },
     },
   },
 }
