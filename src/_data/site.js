e3b312e27d8828ad84aa611d02f62590550f6a67
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2024-03-12 10:26:15 +0800

    Added additional information

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,13 +3,68 @@ const {
   MICROSOFT_SITE_VERIFICATION,
   EMAIL_ADDRESS,
   RESUME,
+  WP_SITE_URL,
 } = require('../../env')
 
-module.exports = {
-  title: 'Rotsen Mark Acob - Jamstack Developer',
-  description: 'Web developer, self-proclaimed introvert and UNIX enthusiast.',
-  google_site_verification: GOOGLE_SITE_VERIFICATION,
-  microsoft_site_verification: MICROSOFT_SITE_VERIFICATION,
-  resume: RESUME,
-  email_address: EMAIL_ADDRESS,
+const GRAPHQL_URL = `${WP_SITE_URL}/graphql`
+const Axios = require('axios')
+const { setupCache } = require('axios-cache-interceptor')
+
+const axios = Axios.defaults.cache ? Axios : setupCache(Axios)
+
+async function requestInformation() {
+  let siteInfo = []
+
+  const headers = {
+    'content-type': 'application/json',
+    Accept: 'application/json',
+  }
+
+  const graphqlQuery = {
+    operationName: 'requestInformation',
+    query: `query requestInformation {
+      page(idType: DATABASE_ID, id: 439) {
+        title
+        content,
+        seo {
+          metaDesc,
+          title
+        },
+        socialMediaInformation {
+          socialMedia {
+            social {
+              name,
+              url
+            }
+          }
+        }
+      }
+    }`,
+  }
+
+  const response = await axios({
+    url: GRAPHQL_URL,
+    method: 'POST',
+    headers: headers,
+    data: graphqlQuery,
+  }).catch(function (error) {
+    console.log(error.toJSON())
+  })
+
+  siteInfo = siteInfo.concat(response.data.data.page)
+
+  const siteInfoFormatted = siteInfo.map((item) => {
+    return {
+      title: item.seo.title,
+      description: item.seo.metaDesc,
+      google_site_verification: GOOGLE_SITE_VERIFICATION,
+      microsoft_site_verification: MICROSOFT_SITE_VERIFICATION,
+      resume: RESUME,
+      email_address: EMAIL_ADDRESS,
+    }
+  })
+
+  return siteInfoFormatted
 }
+
+module.exports = requestInformation
