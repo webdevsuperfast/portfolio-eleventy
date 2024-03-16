484bcb6605ae41d2f6a5777afb6077181d9db1be
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2024-03-16 12:47:35 +0800

    Removed unneeded file

--- src/_scripts/mixitup.js
+++
@@ -1,9 +0,0 @@
-import mixitup from 'mixitup';
-
-const containerEl = document.querySelector('.collections');
-const mixer = mixitup(containerEl, {
-  callbacks: {
-    onMixStart: function(state,futureState){
-    }
-  }
-});
