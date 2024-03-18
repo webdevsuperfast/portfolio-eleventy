70631dbae6045c9fb3d97301e3d1822fb4eaf38a
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2024-03-18 12:53:14 +0800

    Added close button

--- src/_scripts/overlay.js
+++ src/_scripts/overlay.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
   const openContactButton = document.getElementById('open-contact')
   const contactOverlay = document.getElementById('contact')
+  const closeContactButton = document.getElementById('close-contact')
 
   openContactButton.addEventListener('click', (e) => {
     e.preventDefault()
     contactOverlay.classList.remove('hidden')
+  })
+
+  closeContactButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    contactOverlay.classList.add('hidden')
+    console.log('test')
   })
 })
