05cb1105ccf834bcf3aa1a6a2ef244a9a4ce852d
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2024-03-18 14:41:06 +0800

    Testing for errors

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -3,7 +3,10 @@ export async function onRequestPost(context) {
     return await handleRequest(context)
   } catch (e) {
     console.log(e)
-    return new Response('Error sending message', { status: 500 })
+    return new Response(
+      `Error sending message ${context} ${env.TURNSTILE_SECRET}`,
+      { status: 500 }
+    )
   }
 }
 
