4f7c5ebc097412e64db31d23d2dea2d21720021d
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2024-03-21 08:03:30 +0800

    Load turnstile script on button click

--- src/_scripts/overlay.js
+++ src/_scripts/overlay.js
@@ -3,11 +3,36 @@ document.addEventListener('DOMContentLoaded', () => {
   const contactOverlay = document.getElementById('contact')
   const closeContactButton = document.getElementById('close-contact')
 
+  let scriptElement // Variable to store the script element
+
+  const createScript = (src) => {
+    const script = document.createElement('script')
+
+    script.src = src
+    script.async = true
+    script.defer = true
+    return script
+  }
+
   const toggleContactOverlay = (e) => {
     e.preventDefault()
 
     contactOverlay.classList.toggle('hidden')
     contactOverlay.classList.toggle('flex')
+
+    if (contactOverlay.classList.contains('flex')) {
+      // Load script if overlay is opened
+      scriptElement = createScript(
+        'https://challenges.cloudflare.com/turnstile/v0/api.js'
+      )
+      document.head.appendChild(scriptElement)
+    } else {
+      // Remove script if overlay is closed
+      if (scriptElement) {
+        scriptElement.remove()
+        scriptElement = null // Clear reference
+      }
+    }
   }
 
   openContactButton.addEventListener('click', toggleContactOverlay)
